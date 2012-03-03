e286973cb572df20e5a5d1c3b16501276d8054dc
moz <blue.repliroid@gmail.com>
2012-03-03 21:58:05 +0900

    add general setting

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -5,3 +5,16 @@
                          )
                         load-path))
 
+
+; 一般的な設定
+
+;; emacsclient からのファイルオープンを受け付ける
+(server-start)
+;; 初期画面を表示させない
+(setq inhibit-startup-screen t)
+;; scratch バッファに何も表示しない
+(setq initial-scratch-message "")
+;; Message バッファを表示させない
+(setq message-log-max nil)
+;; バックアップファイルが生成されないようにする
+(setq backup-inhibited t)
